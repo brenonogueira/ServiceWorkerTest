5bb7bbe3f3f779ce880bc97cfe359c89caa12c0b
Breno Nogueia Araújo <breno.nog.araujo@hotmail.com>
2023-10-09 23:40:22 -0400

    Add project files.

---
+++ ServiceWorkerTest/BackgroundWorkerService.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+
+public class BackgroundWorkerService : BackgroundService
+{
+
+    readonly ILogger<BackgroundWorkerService> _logger;
+
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service started...");   
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service stoped...");
+        return Task.CompletedTask;
+    }
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation($"Worker running at: {DateTimeOffset.Now}");
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
